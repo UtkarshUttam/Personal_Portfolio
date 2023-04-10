7887a28bec6320facf8c19e8718b39e395256bb9
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2023-04-10 20:53:46 +0530

    Revamp commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let navContainer = document.getElementById("navbarSupportedContent");
+// let navOptions = navContainer.getElementsByClassName("nav-item");
+
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (let i = 0; i < navOptions.length; i++) {
+//   navOptions[i].addEventListener("click", function() {
+//     let current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
